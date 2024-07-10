b159da3db8cd24e2e957f193e7794ebb6bbd1e18
Henrique <henrique.dias@gruponewway.com.br>
2024-07-10 08:45:46 -0300

    chore(main): fix build
    # Files
    NEW:
    EDITED: nextjs/kanban01/src/app/_components/Board.tsx
    DELETED: nextjs/kanban01/src/app/_components/_dynamic-imports.tsx

--- nextjs/kanban01/src/app/_components/Board.tsx
+++ nextjs/kanban01/src/app/_components/Board.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { faker } from '@faker-js/faker';
 import { UncontrolledBoard } from '@caldwell619/react-kanban';
 
-export default function Board({ initialBoard }: { initialBoard?: { columns: { id: bigint, title: string, cards: { id: bigint, title: string, description: string }[] }[] } } = {}) {
-    const board: { columns: { id: bigint, title: string, cards: { id: bigint, title: string, description: string }[] }[] } = initialBoard || {
+export default function Board({ initialBoard }: any) {
+    const board: any = initialBoard || {
         columns: Array.from({ length: faker.number.int({ min: 3, max: 5 }) }, () => ({
             id: faker.string.uuid(),
             title: faker.lorem.words(),
--- nextjs/kanban01/src/app/_components/_dynamic-imports.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-
-const ReactKanban = dynamic(() => import('@lourenci/react-kanban'), { ssr: false })
-
-const allImports = { ReactKanban }
-export default allImports
-
-
